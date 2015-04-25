418d565b8cf8db4f63e811d2066b8eea7474afcb
John N. Mandalakas <j.n.mandalakas@gmail.com>
2015-04-25 18:39:19 -0500

    add require_login helper method to controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :require_login
+
   def current_user
     @_user || User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_login
+    redirect_to new_session_path, notice: 'You must be logged in to access this section.' unless current_user
   end
 end
