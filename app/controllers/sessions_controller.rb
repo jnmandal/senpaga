8eefae6a49def67ecc6d70fbc2320a20c6043d1d
John N. Mandalakas <j.n.mandalakas@gmail.com>
2015-04-28 14:47:39 -0500

    add error notices for login and wishlist creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,18 +6,16 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_name(params[:user][:name]).try(:authenticate, params[:user][:password])
     if @user
-      puts "success"
       session[:user_id] = @user.id
-      redirect_to "/"
+      redirect_to :root
     else
-      puts "fail"
-      @user = User.new
-      render "new"
+      flash[:notice] = 'Incorrect username or password.'
+      render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to :root
+    redirect_to :root, notice: 'Logged out.'
   end
 end
