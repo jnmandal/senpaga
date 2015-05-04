972478438e108bc9f4ac10f02fdc7d57aad36674
John N. Mandalakas <j.n.mandalakas@gmail.com>
2015-05-03 21:27:20 -0500

    add better wishlist view with books in rows of three

--- app/assets/javascripts/components/book.js.jsx
+++ app/assets/javascripts/components/book.js.jsx
@@ -6,26 +6,33 @@ var Book = React.createClass({
     }
     return (
       <div className="book">
-        <p>
-          <img src={"http://covers.openlibrary.org/b/isbn/"+this.props.asin+"-M.jpg"} />
-        </p>
-        <p>
-          <strong>asin: </strong>
-          <a href={"http://www.amazon.com/dp/"+this.props.asin}>{this.props.asin}</a>
-        </p>
-        <p>
-          <strong>title: </strong>
-          {this.props.title}
-        </p>
-        <p>
-          <strong>author: </strong>
-          {this.props.author}
-        </p>
-        <p>
-          <strong>price: </strong>
-          {this.props.price}
-        </p>
-        <span dangerouslySetInnerHTML={generateOLReadLink(this.props.asin)} />
+        <div className="pull-left">
+          <p>
+            <img className="bookCover pull-left" src={"http://covers.openlibrary.org/b/isbn/"+this.props.asin+"-M.jpg"} alt="book cover art courtesy of openlibrary"/>
+          </p>
+        </div>
+        <div className="pull-right">
+          <p>
+            <strong>asin: </strong>
+            <a href={"http://www.amazon.com/dp/"+this.props.asin}>{this.props.asin}</a>
+          </p>
+          <p>
+            <strong>title: </strong>
+            {this.props.title}
+          </p>
+          <p>
+            <strong>author: </strong>
+            {this.props.author}
+          </p>
+          {/*<p>
+            <strong>price: </strong>
+            {this.props.price}
+          </p>*/}
+          <p>
+            <strong>open lib: </strong>
+            <span dangerouslySetInnerHTML={generateOLReadLink(this.props.asin)} />
+          </p>
+        </div>
       </div>
     );
   }
--- app/assets/javascripts/components/book_list.js.jsx
+++ app/assets/javascripts/components/book_list.js.jsx
@@ -5,9 +5,33 @@ var BookList = React.createClass({
           <Book asin={book.asin} title={book.title} author={book.author} price={book.price} />
         );
     });
+
+    function rowify (bookNodes) {
+      rows = []
+      for (i=1; i <= bookNodes.length; i++) {
+        if (i % 3 === 0) {
+          books = bookNodes.slice((i-3), i)
+          rows.push (
+            <div className="row">
+              <div className="col-xs-4">
+                {books[0]}
+              </div>
+              <div className="col-xs-4">
+                {books[1]}
+              </div>
+              <div className="col-xs-4">
+                {books[2]}
+              </div>
+            </div>
+          );
+        }
+      }
+      return rows;
+    }
+
     return (
       <div className="bookList">
-        {bookNodes}
+        {rowify(bookNodes)}
       </div>
     );
   }
