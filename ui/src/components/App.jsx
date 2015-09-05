95507faf8e5b32e14ce31e3f384fb4f04632a683
John N. Mandalakas <j.n.mandalakas@gmail.com>
2015-09-05 15:36:33 -0500

    Change demo wishlist endpoint to heroku-deployed version of API

--- ui/src/components/App.jsx
+++ ui/src/components/App.jsx
@@ -3,7 +3,7 @@ import Wishlist from './Wishlist'
 
 class App extends React.Component {
   render () {
-    return (<Wishlist url="http://localhost:9393/wishlists/2" />)
+    return (<Wishlist url="https://senpaga-dev.herokuapp.com/wishlists/2" />)
   }
 }
 
